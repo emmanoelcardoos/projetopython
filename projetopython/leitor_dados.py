0b8334c6d95227f44b73038ff02681503326e512
emmanoelcardoos <Emmanoel.cardoso09@gmail.com>
2024-12-16 18:49:33 +0000

    Envio inicial para o novo repositório

--- projetopython/leitor_dados.py
+++ projetopython/leitor_dados.py
@@ -1,20 +1,21 @@
-import pandas as pd
+import csv
 import importlib.resources as resources
 
 
 def ler_dados():
     """
     Lê o arquivo CSV da pasta de dados de forma relativa.
-    :return: Dados lidos no formato DataFrame.
+    :return: Dados lidos no formato de lista de dicionários.
     """
     try:
         # Acessando o arquivo CSV com base no pacote correto
         with resources.open_text("projetopython.dados", "tweets.csv") as arquivo_csv:
-            dados = pd.read_csv(arquivo_csv)
+            dados = list(csv.DictReader(arquivo_csv))  # Lendo os dados como uma lista de dicionários
         print("Dados lidos com sucesso!")
         return dados
     except Exception as e:
         print(f"Erro ao ler o arquivo: {e}")
         return None
+
 
 
